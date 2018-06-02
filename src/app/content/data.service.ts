5c91b0a133ff2ee29798e998d8143a9ba1ef7bc3
Mikhail Pavlov <pavlov.michael@gmail.com>
2018-06-02 17:02:55 +0500

    upgrade to Angular 6

--- src/app/content/data.service.ts
+++ src/app/content/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs/Subject';
+import { Subject } from 'rxjs';
 import { AppCoords } from '@app/content/app-coords.interface';
 
 @Injectable()
--- src/app/content/lang.service.ts
+++ src/app/content/lang.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable()
 export class LangService {
